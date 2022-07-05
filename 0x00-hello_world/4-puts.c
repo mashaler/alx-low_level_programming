5d7854d41e5e68b9e54996f61183b313269d52a0
mashaler <mashalerolva@gmail.com>
2022-07-05 14:25:31 -0700

    commits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-
 #include <string.h>
-
- 
-
 /**
  *
  *  * main - Entry point
@@ -13,19 +9,8 @@
  *    * Return: Always 0 (Success)
  *
  *     */
-
- 
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-	 
-
 	return (0);
-
 }
-
-
