53c3b0b96615af99e825a17897de34e8d18e175e
mashaler <mashalerolva@gmail.com>
2022-07-13 06:58:37 -0700

    commits

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,18 +17,16 @@ void print_to_98(int n)
 			else
 				printf("%d, ", n);
 			n = n + 1;
-
 		}
-		else if (n > 98)
+	} else if (n > 98)
+	{
+		while (n >= 98)
 		{
-			while (n >= 98)
-			{
-				if (n == 98)
-					printf("%d\n", n);
-				else
-					printf("%d, ", n);
-				n = n - 1;
-			}
+			if (n == 98)
+				printf("%d\n", n);
+			else
+				printf("%d, ", n);
+			n = n - 1;
 		}
 	}
 }
