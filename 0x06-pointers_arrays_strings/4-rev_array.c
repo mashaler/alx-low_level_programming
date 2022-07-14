cffa5e2f2beb9850329a87c6432eef124b26a66d
mashaler <mashalerolva@gmail.com>
2022-07-14 14:32:06 -0700

    commits

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,16 +7,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
+	int tmp, index;
 
-	p = a;
-	for (i = 0; i < n; i++)
-		p++;
-	for (k = 0; k < i / 2; k++)
+	for (index = n - 1; index > n / 2; index++)
 	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
 	}
 }
