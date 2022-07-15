2373a439ba808864673dd2e3f1e90c27c770e160
mashaler <mashalerolva@gmail.com>
2022-07-15 02:07:13 -0700

    commits

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,12 +15,14 @@ char *cap_string(char *x)
 	while (x[a])
 	{
 		i = 0;
+
 		while (i < len)
 		{
 			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
 				x[a] = x[a] - 32;
 			i++;
 		}
+		a++;
 	}
-	a++;
+	return (x);
 }
