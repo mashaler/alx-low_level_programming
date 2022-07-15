1b2cafefa9b19bb271b0ce935575dc05f32eaab9
mashaler <mashalerolva@gmail.com>
2022-07-15 12:06:22 -0700

    commits

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,12 +12,7 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
-	}
-	else
-	{
-		num = n;
-	}
+		num = -n;
 	if ((num / 10) > 0)
 		print_number(num / 10);
 	_putchar((num % 10) + '0');
