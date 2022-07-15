148b679cb460dbd550ad7bd0974114cfb1ff22d9
mashaler <mashalerolva@gmail.com>
2022-07-15 02:39:56 -0700

    commits

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 -function that encodes string using rot13
+ * @s: string
+ * Return: encodes string
+ */
+
+char *rot13(char *s)
+{
+	int x, y;
+
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] =	"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] = '\0'; x++)
+	{
+		for (y = 0; input[y] != '\0'; y++)
+		{
+			if (s[x] == input[y])
+			{
+				s[x] = output[y];
+			}
+		}
+	}
+}
