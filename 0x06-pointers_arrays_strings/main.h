701d6e22dd9d770fa206ca23a9699f53577c066f
mashaler <mashalerolva@gmail.com>
2022-07-15 14:07:36 -0700

    commits

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include<stdio.h>
+void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 int _putchar(char c);
