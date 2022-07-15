18d7741dae7dd5e923b407fe51e3b99849cbc3cf
mashaler <mashalerolva@gmail.com>
2022-07-15 12:29:55 -0700

    commits

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include<stdio.h>
+void print_number(int n);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
