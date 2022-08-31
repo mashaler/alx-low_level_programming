d398ff1c0934996b6e16f02ce903e9da33d1fa7c
mashaler <mashalerolva@gmail.com>
2022-08-31 12:54:45 -0700

    commits

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -47,12 +47,12 @@ size_t looped_listint_len(const listint_t *head)
 		hare = (hare->next)->next;
 	}
 	return (0);
-	/**
-	 *  print_listint_safe - Prints a listint_t list safely.
-	 *  @head: A pointer to the head of the listint_t list.
-	 *
-	 *  Return: The number of nodes in the list.
-	 */
+/**
+*  print_listint_safe - Prints a listint_t list safely.
+*  @head: A pointer to the head of the listint_t list.
+*
+*  Return: The number of nodes in the list.
+*/
 	size_t print_listint_safe(const listint_t *head)
 	{
 		size_t nodes, index = 0;
